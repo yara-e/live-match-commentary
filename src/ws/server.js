9c103073eac9ed20ae6673b72c9ccf5d3a7c9194
yara-e <yaraelsayed237@gmail.com>
2026-04-13 19:34:04 +0200

    implements coderabbit fixes

--- src/ws/server.js
+++ src/ws/server.js
@@ -1,3 +1,4 @@
+import { setInterval } from "timers/promises";
 import { WebSocket, WebSocketServer } from "ws"
 
 function sendJson(socket, payload){
@@ -8,7 +9,7 @@ function sendJson(socket, payload){
 
 function broadcast(wss,payload){
     for(const client of wss.clients){
-            if(client.readyState != WebSocket.OPEN) return;
+            if(client.readyState != WebSocket.OPEN) continue;
             client.send(JSON.stringify(payload));
     }
 }
@@ -21,9 +22,19 @@ export function attachWebSocketServer(server){
     })
 
     wss.on('connection',(socket)=>{
+
+        socket.isAlive = true;
+        socket.on('pong',()=>{socket.isAlive =true});
         sendJson(socket,{type: 'welcome'});
         socket.on('error', console.error);
     });
+    const interval = setInterval(()=>{
+        wss.clients.forEach((ws)=>{
+            if (ws.isAlive === false) return ws.terminate();
+            ws.isAlive=false;
+            ws.ping();
+        })
+    } , 30000);
 
     function broadcastMatchCreated(match){
         broadcast(wss, {type: 'match_created', data:match});
